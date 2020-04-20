7959cd5c6e74d3c88650b718eef543b2972d9909
Rucha <ruchammantri@gmail.com>
2020-04-21 01:41:40 +0530

    comment

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import mimetypes
 import os
 import sys
 
@@ -20,6 +19,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-mimetypes.init()
-mimetypes.types_map['.css'] = 'text/css'
