139894db59fcbdc675cfedd33525e190e5121d5d
Rucha <ruchammantri@gmail.com>
2020-04-21 01:29:11 +0530

    comment

--- recipes/views.py
+++ recipes/views.py
@@ -11,12 +11,12 @@ class CreateRecipeView(CreateView):
     form_class = RecipeForm
 
 class RecipeListView(ListView):
-    model = Entry
+    model = Recipe
     template_name = 'entries/recipes_list.html'
     context_object_name = "recipes_list"
 
 class RecipeEntryView(DetailView):
-    model = Entry
+    model = Recipe
     template_name = 'entries/recipes_details.html'
     context_object_name = "recipe_details"
 
