dc3b2d8602fb26b7f1035067914d854eefc78cef
Rucha Mantri <ruchamantri@users.noreply.github.com>
2020-04-21 17:28:20 +0530

    comment

--- entries/models.py
+++ entries/models.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
-from model_utils.fields import SplitField
 
 
 class Entry(models.Model):
     entry_title = models.CharField(max_length=50)
-    entry_text = SplitField()
+    entry_text = models.TextField()
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
